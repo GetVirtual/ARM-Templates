3b508d5784baf6dbc7f27d31753f495a1b2a1f43
Jürgen Lobenz <juergen.lobenz@microsoft.com>
2019-02-05 13:05:45 +0100

    Fix DSC

--- MultiRegionHA/DSC/IIS.ps1
+++ MultiRegionHA/DSC/IIS.ps1
@@ -15,19 +15,19 @@ Configuration IIS {
         Script webrequest {
             SetScript = {
                 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                Invoke-WebRequest "https://github.com/GetVirtual/ARM-Templates/raw/master/MultiRegionHA/DSC/index.html" -OutFile "C:\inetpub\wwwroot\index.html"
+                Invoke-WebRequest "https://github.com/GetVirtual/ARM-Templates/raw/master/MultiRegionHA/DSC/index.html" -OutFile "C:\inetpub\wwwroot\index2.html"
             }
-            TestScript = { Test-Path "C:\inetpub\wwwroot\index.html" }
-            GetScript = { @{ Result = (Get-Content C:\inetpub\wwwroot\index.html) } }       
+            TestScript = { Test-Path "C:\inetpub\wwwroot\index2.html" }
+            GetScript = { @{ Result = (Get-Content C:\inetpub\wwwroot\index2.html) } }       
         }
 
         Script generateIndex {
             SetScript = {
                 $hostname = hostname
-                write-host ("<html><head><title>" + $hostname + "</title></head><body>" + $hostname + "</body></html>") | Out-File "C:\inetpub\wwwroot\index2.html" -Force
+                ("<html><head><title>" + $hostname + "</title></head><body><h1>" + $hostname + "</h1></body></html>") | Out-File "C:\inetpub\wwwroot\index.html" -Force
             }
-            TestScript = { Test-Path "C:\inetpub\wwwroot\index2.html" }
-            GetScript = { @{ Result = (Get-Content C:\inetpub\wwwroot\index2.html) } }       
+            TestScript = { Test-Path "C:\inetpub\wwwroot\index.html" }
+            GetScript = { @{ Result = (Get-Content C:\inetpub\wwwroot\index.html) } }       
         }
     }
 }
