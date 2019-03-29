a49d9410c4e86bb2b4dbd224b185adffe13547ef
Jürgen Lobenz <juergen.lobenz@microsoft.com>
2019-03-29 12:07:37 +0100

    AdminCenterVMs

---
+++ Windows-AdminCenter/azuredeploy.ps1
@@ -0,0 +1,12 @@
+$RG = "AdminCenter"
+
+Get-AzSubscription -SubscriptionName "Microsoft Azure Internal Consumption" -ErrorAction SilentlyContinue | Set-AzContext
+
+Remove-AzResourceGroup -Name $RG -Force -ErrorAction SilentlyContinue
+New-AzResourceGroup -Name $RG -Location "West Europe"
+
+Start-Sleep -Second 5
+
+#Test-AzResourceGroupDeployment -ResourceGroupName "ARM" -TemplateFile .\azuredeploy.json
+
+New-AzResourceGroupDeployment -Name "Deployment" -ResourceGroupName $RG -TemplateFile .\azuredeploy.json -Verbose
