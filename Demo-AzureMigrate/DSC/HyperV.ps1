586495ce45dabba2aca528e9a7c744aa0f2ee2c6
Jürgen Lobenz <juergen.lobenz@microsoft.com>
2019-07-12 19:04:41 +0200

    Fix DSC

--- Demo-AzureMigrate/DSC/HyperV.ps1
+++ Demo-AzureMigrate/DSC/HyperV.ps1
@@ -8,14 +8,7 @@ Configuration HyperV {
         LocalConfigurationManager
         {
             RebootNodeIfNeeded = $true
-        }
-
-        MsiPackage InstallWindowsAdminCenter
-        {
-            ProductId = '{4FAE3A2E-4369-490E-97F3-0B3BFF183AB9}'
-            Path      = 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1809.5.msi'
-            Arguments = "/qn /l*v c:\windows\temp\windowsadmincenter.msiinstall.log SME_PORT=6516 SSL_CERTIFICATE_OPTION=generate"
-            Ensure    = 'Present'
+            ActionAfterReboot = 'ContinueConfiguration'
         }
 
         WindowsFeature Hyper-V {
@@ -25,20 +18,20 @@ Configuration HyperV {
 
         }
 
-        WindowsFeature Failover-Clustering {
-            Ensure = "Present"
-            Name   = "Failover-Clustering"
-        }
-
         WindowsFeature Hyper-V-Powershell {
             Ensure = "Present"
             Name   = "Hyper-V-Powershell"
             IncludeAllSubFeature = $true
         }
 
+        WindowsFeature Failover-Clustering {
+            Ensure = "Present"
+            Name   = "Failover-Clustering"
+        }
+
         WindowsFeature Multipath-IO {
             Ensure = "Present"
-            Name   = "Multipath-IO "
+            Name   = "Multipath-IO"
             IncludeAllSubFeature = $true
         }
 
